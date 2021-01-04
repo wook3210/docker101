25f672f69a783c2d5c66d5c3bf3e55c401613242
wook3210 <wook3210@gmail.com>
2021-01-04 14:27:56 +0900

    docker-compose --build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react 이제 디자인 쪽으로 잠깐 넘어가 보자~
+
         </a>
       </header>
     </div>
